b097e9dc74b5f66cacc78e023e8463d365c2446f
Anton Litus <littox90@gmail.com>
2022-12-05 02:00:27 +0300

    sprint_2: cleanup

--- src/pages/chat/components/chat-item/index.ts
+++ src/pages/chat/components/chat-item/index.ts
@@ -1,10 +1,6 @@
 import template from './chat-item.hbs';
 import Component from '../../../../utils/Component';
 
-// export type ChatProps = {
-//   chat: Record<string, any>;
-// };
-
 export default class ChatItem extends Component {
   render(): DocumentFragment {
     return this.compile(template, { ...this.props });
