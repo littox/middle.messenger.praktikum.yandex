3bfa5f2033cef8534be96d295487d25ab67fc04a
Anton Litus <littox90@gmail.com>
2022-12-15 03:15:36 +0300

    sprint_2: PORT const fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 const distDir = path.join(__dirname, 'dist');
 
 app.use(express.static(`${__dirname}/dist/`));
@@ -10,6 +9,7 @@ app.use(express.static(`${__dirname}/dist/`));
 app.use('/', (req, res) => {
   res.sendFile(path.join(distDir, 'index.html'));
 });
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT || PORT}!`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
