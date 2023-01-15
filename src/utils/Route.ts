52fb22df824dfef2301ee12e119b76b6bdc0f434
Anton Litus <littox90@gmail.com>
2023-01-15 06:36:20 +0300

    sprint_4: router refactoring

--- src/utils/Route.ts
+++ src/utils/Route.ts
@@ -1,17 +1,4 @@
 import { Component } from './Component';
-import { NotFoundException } from './Exceptions';
-
-export enum Routes {
-  NotFound = '/404',
-  Error = '/500',
-  Chat = '/messenger',
-  Profile = '/settings',
-  ProfilEdit = '/settings-edit',
-  PasswordEdit = '/settings-edit-password',
-  Registration = '/sign-up',
-  Logout = '/logout',
-  Index = '/',
-}
 
 function isEqual(lhs: string, rhs: string): boolean {
   return lhs === rhs;
@@ -31,7 +18,7 @@ function render(query: string, block: Component) {
   return root;
 }
 
-class Route {
+export class Route {
   private block: Component | null = null;
 
   constructor(
@@ -57,75 +44,3 @@ class Route {
     }
   }
 }
-
-class Router {
-  private static __instance: Router;
-
-  private routes: Route[] = [];
-
-  private currentRoute: Route | null = null;
-
-  private history = window.history;
-
-  constructor(private readonly rootQuery: string) {
-    if (Router.__instance) {
-      return Router.__instance;
-    }
-
-    this.routes = [];
-
-    Router.__instance = this;
-  }
-
-  public use(pathname: string, block: typeof Component) {
-    const route = new Route(pathname, block, this.rootQuery);
-    this.routes.push(route);
-
-    return this;
-  }
-
-  public start() {
-    window.onpopstate = (event: PopStateEvent) => {
-      const target = event.currentTarget as Window;
-
-      this._onRoute(target.location.pathname);
-    };
-
-    this._onRoute(window.location.pathname);
-  }
-
-  public go(pathname: string) {
-    this.history.pushState({}, '', pathname);
-
-    this._onRoute(pathname);
-  }
-
-  public back() {
-    this.history.back();
-  }
-
-  public forward() {
-    this.history.forward();
-  }
-
-  private _onRoute(pathname: string) {
-    const route = this.getRoute(pathname);
-
-    if (!route) {
-      throw new NotFoundException(`${pathname} page not found`);
-    }
-
-    if (this.currentRoute && this.currentRoute !== route) {
-      this.currentRoute.leave();
-    }
-
-    this.currentRoute = route;
-
-    route.render();
-  }
-
-  private getRoute(pathname: string) {
-    return this.routes.find((route) => route.match(pathname));
-  }
-}
-export const router = new Router('#app');
--- src/utils/Router.ts
+++ src/utils/Router.ts
@@ -1,5 +1,6 @@
 import { Component } from './Component';
 import { NotFoundException } from './Exceptions';
+import { Route } from './Route';
 
 export enum Routes {
   NotFound = '/404',
@@ -11,51 +12,6 @@ export enum Routes {
   Registration = '/sign-up',
   Logout = '/logout',
   Index = '/',
-}
-
-function isEqual(lhs: string, rhs: string): boolean {
-  return lhs === rhs;
-}
-
-function render(query: string, block: Component) {
-  const root = document.querySelector(query);
-
-  if (root === null) {
-    throw new Error(`root not found by selector "${query}"`);
-  }
-
-  root.innerHTML = '';
-
-  root.append(block.getContent()!);
-
-  return root;
-}
-
-class Route {
-  private block: Component | null = null;
-
-  constructor(
-    private pathname: string,
-    private readonly blockClass: typeof Component,
-    private readonly query: string,
-  ) {
-  }
-
-  leave() {
-    this.block = null;
-  }
-
-  match(pathname: string) {
-    return isEqual(pathname, this.pathname);
-  }
-
-  render() {
-    if (!this.block) {
-      this.block = new this.blockClass({});
-
-      render(this.query, this.block);
-    }
-  }
 }
 
 class Router {
